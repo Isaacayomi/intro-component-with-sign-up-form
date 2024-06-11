4625c880e73781531ad1b1407ea2ff7eb181ef5d
Isaac Ayomide <okunlolaa3@outlook.com>
2024-06-11 21:49:08 +0100

    wip

---
+++ script.js
@@ -0,0 +1,7 @@
+const firstName = document.querySelector("#firstName");
+const lastName = document.querySelector("#firstName");
+const email = document.querySelector("#firstName");
+const password = document.querySelector("#firstName");
+const submitBtn = document.querySelector(".cta__btn");
+
+
